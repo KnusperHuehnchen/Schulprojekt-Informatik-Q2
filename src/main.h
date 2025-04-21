0ab65f4e88cfbebbca69d573c597e1b670435711
Luka <luka.hofmann24@icloud.com>
2025-04-21 19:35:54 +0200

    Der Starteffekt läuft nun mithilfe der millis gleichzeitig ab.

--- src/main.h
+++ src/main.h
@@ -5,8 +5,15 @@
 #define MAIN_H
 
 #include  <Adafruit_NeoPixel.h>
+
 Adafruit_NeoPixel pixels(30, 3, NEO_GRB + NEO_KHZ800);
 int button = 2;
+
+unsigned long previousMillis = 0;
+const long interval = 100;
+
+int r = 30;
+int l = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -14,17 +21,24 @@ void setup() {
     pixels.clear();
     pixels.setBrightness(255);
     pinMode(button,INPUT_PULLUP);
-    for (int i = 30; i >= 15; i--) {
-        pixels.setPixelColor(i, pixels.Color(0, 0, 255));
-        pixels.show();
-        delay(100);
-    }
-    for (int i = 0; i < 15; i++) {
-        pixels.setPixelColor(i, pixels.Color(0, 0, 255));
-        pixels.show();
-        delay(100);
-    }
-    delay(2500);
 }
+void loop() {
+    unsigned long currentMillis = millis();
 
+    if (currentMillis - previousMillis >= interval) { //diese wird nur einmal ausgeführt !
+        previousMillis = currentMillis;
+
+        if (r >= 15) {
+            pixels.setPixelColor(r, pixels.Color(0, 0, 255));
+            r--;
+        }
+
+        if (l < 15) {
+            pixels.setPixelColor(l, pixels.Color(0, 0, 255));
+            l++;
+        }
+
+        pixels.show();
+    }
+}
 #endif //MAIN_H
