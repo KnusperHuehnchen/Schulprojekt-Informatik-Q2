03792c89a41bbc5b9a4286e1eccba68ab23745c9
Luka <luka.hofmann24@icloud.com>
2025-05-05 23:53:05 +0200

    Code nochmal von neu geschrieben (im Geschi Unterricht)

--- src/main.h
+++ src/main.h
@@ -8,47 +8,42 @@
 
 Adafruit_NeoPixel pixels(30, 3, NEO_GRB + NEO_KHZ800);
 int button = 2;
-
-unsigned long previousMillis = 0;
-const long interval = 100;
-
+unsigned long previousMillis = 0; //überprüfen !!!
 int animation = 0;
+const long interval = 100; //überprüfen !!!
 int r = 30;
 int l = 0;
 
-void setup() {
-    pixels.begin();
-    pixels.clear();
-    pixels.setBrightness(255);
-    pinMode(button,INPUT_PULLUP);
+void setup () {
+    pixels.begin ();
+    pinMode(button, INPUT_PULLUP);
 }
-void loop() {
+
+void loop () {
     if (animation == 0) {
         unsigned long currentMillis = millis();
-
         if (currentMillis - previousMillis >= interval) {
             previousMillis = currentMillis;
-
             if (r >= 15) {
-                pixels.setPixelColor(r, pixels.Color(0, 0, 255));
+                pixels.setPixelColor(r, pixels.Color(0, 250, 0));
                 r--;
             }
             if (l < 15) {
-                pixels.setPixelColor(l, pixels.Color(0, 0, 255));
+                pixels.setPixelColor(l, pixels.Color(0, 250, 0));
                 l++;
             }
             pixels.show();
-            if (r == 14 && l == 15){
+            if (r== 14 && l == 15) {
                 animation = 1;
             }
         }
     }
-    else  {
+    else {
         if (digitalRead(button) == 0) {
             for (int i = 0; i < 30; i++) {
-                pixels.setPixelColor(i, pixels.Color(0, 0, 255));
-                pixels.show();
+                pixels.setPixelColor(i, pixels.Color(0, 255, 0));
             }
+            pixels.show();
         }
         else {
             pixels.clear();
