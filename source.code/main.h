018e72a79432a32a33fb03946b989685590d3308
Luka <luka.hofmann24@icloud.com>
2025-05-31 02:33:29 +0200

    Jetzt mit einem LED Streifen 60 LEDs/m, Flankenerkennung, bug fixes

--- source.code/main.h
+++ source.code/main.h
@@ -6,7 +6,7 @@
 
 #include <Adafruit_NeoPixel.h>
 //pins
-Adafruit_NeoPixel pixels(30, 3, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(60, 3, NEO_GRB + NEO_KHZ800);
 int piezo = 2;
 int potiR = 4; //ESP32 4
 int potiG = 5; //ESP32 5
@@ -14,16 +14,16 @@ int potiB = 6; //ESP32 6
 int button = 11;
 
 //millis
-unsigned long previousMillis = 0; //überprüfen !!!
-const long interval = 100; //überprüfen !!!
+unsigned long previousMillis = 0;
+const long interval = 100;
 
 //variablen
 int stay_on = 0;
 int animation = 0;
-int r = 30;
+int r = 60;
 int l = 0;
 int lever = 0;
-
+int old = 1;
 void setup () {
     pixels.begin ();
     pinMode(button, INPUT_PULLUP);
@@ -36,52 +36,49 @@ void loop () {
         int red = analogRead(potiR)/15;
         int green = analogRead(potiG)/15;
         int blue = analogRead(potiB)/15;
-if (digitalRead(button) == 0 && lever == 0) {
+  if (digitalRead(button) == 0 && old == 1 && lever == 0) {
   lever ++;
   animation = 0;
   stay_on = 0;
-  r = 30;
+  r = 60;
   l = 0;
-  delay(200);
   }
-else if (digitalRead(button) == 0 && lever == 1) {
+else if (digitalRead(button) == 0 && old == 1 && lever == 1) {
     lever ++;
-    delay(200);
 }
-else if (digitalRead(button) == 0 && lever == 2) {
+else if (digitalRead(button) == 0 && old == 1 && lever == 2) {
     lever = 0;
-    delay(200);
 }
+old = digitalRead(button);
   switch (lever) {
     case 0:
-      for (int i = 0; i < 30; i++) {
+      for (int i = 0; i < 60; i++) {
             pixels.setPixelColor(i, pixels.Color(0, 0, 255));
       }
       pixels.show();
-      Serial.println(animation);
       break;
     case 1:
         if (animation == 0) {
             unsigned long currentMillis = millis();
             if (currentMillis - previousMillis >= interval) {
                 previousMillis = currentMillis;
-                if (r >= 15) {
+                if (r >= 30) {
                     pixels.setPixelColor(r, pixels.Color(red, green ,blue));
                     r--;
                 }
-                if (l < 15) {
+                if (l < 30) {
                     pixels.setPixelColor(l, pixels.Color(red, green ,blue));
                     l++;
                 }
                 pixels.show();
-                if (r== 14 && l == 15) {
+                if (r== 29 && l == 30) {
                     animation = 1;
                 }
             }
         }
         else {
             if (analogRead(piezo) > 200 && stay_on == 1){
-               for (int i = 0; i < 30; i++) {
+               for (int i = 0; i < 60; i++) {
                    pixels.setPixelColor(i, pixels.Color(red, green, blue));
                }
                pixels.show();
@@ -91,7 +88,7 @@ else if (digitalRead(button) == 0 && lever == 2) {
                 pixels.show();
             }
       	    else if (analogRead(piezo) > 200 && stay_on == 0){
-                for (int i = 0; i < 30; i++) {
+                for (int i = 0; i < 60; i++) {
                     pixels.setPixelColor(i, pixels.Color(red, green, blue));
                 }
       	        stay_on = 1;
@@ -100,12 +97,11 @@ else if (digitalRead(button) == 0 && lever == 2) {
         }
         break;
     case 2:
-      for (int i = 0; i < 30; i++) {
-            pixels.setPixelColor(i, pixels.Color(0, 255, 0));
-      }
+      for (int i = 0; i < 60; i++) {
+      pixels.clear ();
       pixels.show();
-      Serial.println(animation);
      break;
+      }
   }
 }
 #endif //MAIN_H
